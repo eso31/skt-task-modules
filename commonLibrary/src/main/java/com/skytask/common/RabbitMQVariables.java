6197f78085eae302f6b24aa17e797e31ec638a1b
Dilan Coss <dilanarma@hotmail.com>
2018-09-10 09:52:58 -0600

    Adds annotation component instead of configuration

--- commonLibrary/src/main/java/com/skytask/common/RabbitMQVariables.java
+++ commonLibrary/src/main/java/com/skytask/common/RabbitMQVariables.java
@@ -1,11 +1,11 @@
 package com.skytask.common;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
-@Configuration
+@Component
 @ConfigurationProperties("rabbitmq.config")
 public class RabbitMQVariables {
     private Map<String, String> queues;
